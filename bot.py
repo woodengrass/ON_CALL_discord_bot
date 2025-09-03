153d57f99ade922eb67a0c7331579d7016225dea
woodengrass <161939738+woodengrass@users.noreply.github.com>
2025-09-03 22:53:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,13 +16,13 @@ with open("config/honeypot_config.json", "r") as f:
 
 bot.honeypots = honeypot_config
 
-TEST_GUILD_ID = 1399108525954957442
+TEST_GUILD_ID = 你的伺服器ID
 
 @bot.event
 async def on_ready():
-    guild = discord.Object(id=1399108525954957442)  # 替换成你测试的服务器ID，int类型
+    guild = discord.Object(id=你的伺服器ID)
     await bot.tree.sync(guild=guild)
-    print(f"✅ Slash 命令已同步到服务器，Bot 在线：{bot.user}")
+    print(f"Slash 命令已同步到伺服器，bot上線：{bot.user}")
 
 async def main():
     await bot.load_extension("cogs.honeypot_monitor")
@@ -31,6 +31,7 @@ async def main():
     await bot.load_extension("cogs.people_counting")
     await bot.load_extension("cogs.welcome_listener")
     await bot.load_extension("cogs.cog_load")
+    await bot.load_extension("cogs.delete_listener")
     await bot.start(token)
 
 asyncio.run(main())
